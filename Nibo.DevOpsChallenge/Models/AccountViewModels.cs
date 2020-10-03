b037f5a714ea480416dcb86bffad36d432870bca
Matheus Alves <connect.malves@gmail.com>
2020-10-03 15:04:50 -0300

    Adiciona propriedade de e-mail no ViewModel que falta

--- Nibo.DevOpsChallenge/Models/AccountViewModels.cs
+++ Nibo.DevOpsChallenge/Models/AccountViewModels.cs
@@ -84,6 +84,11 @@ namespace Nibo.DevOpsChallenge.Models
     public class ResetPasswordViewModel
     {
         [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
